6152d8929122f5533489768e30e7cf4860fc8307
anirml <jeppelj@yahoo.dk>
2019-11-19 12:06:31 +0100

    added flight og airport entity

---
+++ src/main/java/entities/Airport.java
@@ -0,0 +1,54 @@
+
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Airport implements Serializable {
+
+    //TODO one to many 
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String IATA;
+    private String placeName;
+
+    public Airport(String IATA, String placeName) {
+        this.IATA = IATA;
+        this.placeName = placeName;
+    }
+
+    public Airport() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIATA() {
+        return IATA;
+    }
+
+    public void setIATA(String IATA) {
+        this.IATA = IATA;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+    
+}
---
+++ src/main/java/entities/Flight.java
@@ -0,0 +1,104 @@
+
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+@Entity
+public class Flight implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date date;
+    private double price;
+    private String link;
+    private Long duration;
+
+    @ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.DETACH)
+    private String origin;
+    @ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.DETACH)
+    private String destination;
+
+    public Flight(String origin, String destination, Date date, double price, String link, Long duration) {
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+        this.price = price;
+        this.link = link;
+        this.duration = duration;
+    }
+
+    public Flight() {
+    }
+       
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+    
+}
