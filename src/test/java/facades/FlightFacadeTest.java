28feaadabb7096df714c070abcacf74ef44790f7
anirml <jeppelj@yahoo.dk>
2019-12-03 12:15:47 +0100

    fix for travis

--- src/test/java/facades/FlightFacadeTest.java
+++ src/test/java/facades/FlightFacadeTest.java
@@ -2,9 +2,6 @@
 package facades;
 
 import dto.FlightDTO;
-import entities.Role;
-import entities.User;
-import static groovy.util.Eval.x;
 import java.io.IOException;
 import java.net.ProtocolException;
 import java.text.ParseException;
@@ -15,24 +12,17 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import static oracle.jrockit.jfr.events.Bits.longValue;
 import org.eclipse.persistence.jpa.jpql.Assert;
-import org.hamcrest.CoreMatchers;
-import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import utils.EMF_Creator;
 
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
 public class FlightFacadeTest {
     
-    
-
-
+   
 
     private static EntityManagerFactory emf;
     private static FlightFacade facade;
